964fe954ed9c660c0357ce77e33c3f3ed2207095
matteo <matteoa1999@gmail.com>
2022-05-24 15:35:27 +0200

    Add external collaboratotrs

---
+++ ext_collaborators/model/src/main/java/com/unicam/newemployee/Email.java
@@ -0,0 +1,19 @@
+package com.unicam.newemployee;
+
+import java.util.Map;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class Email implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+
+		// TODO Auto-generated method stub
+		Map<String, Object> variables = execution.getVariables();
+		execution.getProcessEngineServices().getRuntimeService().createMessageCorrelation("doc_msg")
+				.setVariables(variables).correlate();
+	}
+
+}
